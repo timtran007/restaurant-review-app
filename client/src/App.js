eb8b439d8880215604b053fdcfed9ef2344ce2f9
Tim <timtran007@gmail.com>
2023-01-20 13:34:58 -0600

    add routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        
+        <Nav />
+        <Switch>
+          <Route exact path='/'>
+            <RestaurantContainer />
+          </Route>
+          <Route path='/signup'>
+            <SignupForm />
+          </Route>
+          <Route path='/login'>
+            <LoginForm />
+          </Route>
+          <Route path='/profile'>
+            <ProfileContainer />
+          </Route>
+        </Switch>
     </div>
   );
 }

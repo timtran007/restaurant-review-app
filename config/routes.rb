dddabfe76da686da57f96e44c38dff70bb06fe65
Tim <timtran007@gmail.com>
2023-01-20 14:41:23 -0600

    add model, controller, serializer and index, create, show actions for User

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :restaurants, only: [:index]
+  resources :user, only: [:index, :create, :show]
   # Defines the root path route ("/")
   # root "articles#index"
   

2cae994efe810814d317f0b7107698d33e0ebd9a
Tim <timtran007@gmail.com>
2023-01-22 23:49:29 -0600

    update restaurants route to include show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   resources :reviews
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :restaurants, only: [:index]
-  
-
+  resources :restaurants, only: [:index, :show]
   post '/signup', to: 'users#create'
   get '/me', to: 'users#show'
   

cb22c94a271bdcc292f854fae36e79106f5fe5a9
Tim <timtran007@gmail.com>
2023-01-22 23:40:29 -0600

    add validations for Restaurant attributes

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,9 @@
 class Restaurant < ApplicationRecord
     has_many :reviews
     has_many :users through :reviews
+
+    validates :name, presence: true
+    validates :address, presence: true
+    validates :cuisine, inclusion: { in: ['American', 'Mexican', 'Italian', 'Asian', 'Mediterranean', 'French']}
+    validates :price, numericality: {in: 1..3}
 end

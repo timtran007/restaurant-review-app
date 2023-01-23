c1713542f9a6ff6f8a4208e2cbdacb1a8a5a97dc
Tim <timtran007@gmail.com>
2023-01-22 22:42:10 -0600

    add validation to user: name and username

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
 
     has_many :reviews
     has_many :restaurants through :reviews
+    validates :user_name, presence: true, uniqueness: true
+    validates :name, presence: true
 end

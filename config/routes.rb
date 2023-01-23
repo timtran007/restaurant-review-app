59beb731d4196a6d3a026c60a0275276bae8404f
Tim <timtran007@gmail.com>
2023-01-22 23:44:42 -0600

    update routes for users controller create and show actions

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   resources :reviews
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :restaurants, only: [:index]
-  resources :user, only: [:index, :create, :show]
+  
+
+  post '/signup', to: 'users#create'
+  get '/me', to: 'users#show'
+  
   # Defines the root path route ("/")
   # root "articles#index"
   

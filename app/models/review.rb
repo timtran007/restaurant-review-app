f2ecfe35bd07fef3556d79521bbcc849bc180bc7
Tim <timtran007@gmail.com>
2023-01-22 23:03:04 -0600

    add validations for Review attributes

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
   belongs_to :user
+  validates :headline, presence: true
+  validates :rating, numericality: {in: 1..5}
+  validates :comment, length: {minimum: 50}
 end

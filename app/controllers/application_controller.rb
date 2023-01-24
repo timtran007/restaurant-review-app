b833f5610eef53fb0caebcf1ed65acadad390f13
Tim <timtran007@gmail.com>
2023-01-24 12:48:55 -0600

    add authorize private method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,20 @@ class ApplicationController < ActionController::API
 
     rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found
 
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+
     private
 
-    def render_record_not_found(invalid)
+    def render_record_not_found
         render json: {error: ["record not found"]}, status: :not_found
+    end
+
+    def render_unprocessable_entity(invalid)
+        render json: {errors: invalid.record.errors.full_messages}, status: 422
+    end
+
+    def authorize
+        render json: {errors: ["Unauthorized user"]}, status: 401 unless session.include? :user_id
     end
 
 end

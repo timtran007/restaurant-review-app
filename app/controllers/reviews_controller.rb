f63853eca7841e56f3c7eb25029b806bebb50c62
Tim <timtran007@gmail.com>
2023-01-24 11:49:21 -0600

    add error message for review not found

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,13 @@
 class ReviewsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found
     def index
+        reviews = Review.all
+        render json: reviews
     end
 
     def show
+        review = Review.find_by!(id: params[:id])
+        render json: review
     end
 
     def create
@@ -13,4 +18,12 @@ class ReviewsController < ApplicationController
 
     def destroy
     end
+
+    private
+
+    def render_record_not_found(invalid)
+        binding.break
+        render json: {error: ["record not found"]}, status: :not_found
+    end
+
 end

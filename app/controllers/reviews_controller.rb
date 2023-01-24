e37c2d3d43611a5f32a616679c0e54df4f4f6c02
Tim <timtran007@gmail.com>
2023-01-24 12:07:55 -0600

    update reviews controller action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,17 +6,29 @@ class ReviewsController < ApplicationController
     end
 
     def show
-        review = Review.find_by!(id: params[:id])
+        review = Review.find!(params[:id])
         render json: review
     end
 
     def create
+        review = Review.create(review_params)
+        render json: review, status: :created
     end
 
     def update
+        review = Review.find!(params[:id])
+        review.update(review_params)
+        render json: review
     end
 
     def destroy
+
+    end
+
+    private
+
+    def review_params
+        params.permit(:headline, :rating, :comment, :restaurant_id, :user_id)
     end
 
 

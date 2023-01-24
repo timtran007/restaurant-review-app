1c7e669e47509fcbaf5393c6d07ca99c14a8f466
Tim <timtran007@gmail.com>
2023-01-24 12:51:58 -0600

    update show action and add before_action and skip_before_action logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,25 @@
 class UsersController < ApplicationController
+
+    before_action :authorize
+    skip_before_action :authorize, only: [:create]
+
     def create
+        user = User.create!(user_params)
+        if user &.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: {errors: user.errors.full_messages}
     end
     
     def show
-        user = User.find(params[:id])
+        user = User.find(session[:user_id])
         render json: user
     end
+
+    private
+    def user_params
+        params.permit(:name, :image_url, :user_name, :passowrd, :password_confirmation)
+    end
+
 end

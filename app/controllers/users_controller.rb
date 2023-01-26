21c4b7e870ce4966ecd8a7251e924bbf74d58178
Tim <timtran007@gmail.com>
2023-01-26 09:59:18 -0600

    uncomment test index action and users route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
 
     before_action :authorize
-    skip_before_action :authorize, only: [:create, :index, :show]
+    skip_before_action :authorize, only: [:create]
 
-    def index
-        users = User.all
-        render json: users
-    end
-
+    # def index
+    #     users = User.all
+    #     render json: users
+    # end
+    
     def create
         
         user = User.create!(user_params)

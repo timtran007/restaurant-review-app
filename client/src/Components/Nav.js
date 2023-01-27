d8e80327d2de73ebc84da28bebf4909bf26f31c0
Tim <timtran007@gmail.com>
2023-01-27 11:46:18 -0600

    add NavLinks to the Nav components

--- client/src/Components/Nav.js
+++ client/src/Components/Nav.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Nav(){
     return(
-        <div>
-
-        </div>
+        <nav>
+            <NavLink exact to="/">
+                Home
+            </NavLink>
+            <NavLink to="/users/new">
+                Signup
+            </NavLink>
+            <NavLink to="/login">
+                Login
+            </NavLink>
+            <NavLink to="profile">
+                Profile's Name
+            </NavLink>
+            <NavLink to="logout">
+                Logout
+            </NavLink>
+        </nav>
     )
 }
 

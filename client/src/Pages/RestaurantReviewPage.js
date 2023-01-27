0c822719724b2e2bdb3e827bbeb5e4bf82397106
Tim <timtran007@gmail.com>
2023-01-26 23:17:13 -0600

    add RestaurantReviewPage component to house specific restaurant page and its reviews

---
+++ client/src/Pages/RestaurantReviewPage.js
@@ -0,0 +1,13 @@
+//intention of the page is to show state of the restaurant detail page with all reviews... get request for /restaurants/:id/reviews
+
+import React from "react";
+
+function RestaurantReviewPage() {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default RestaurantReviewPage;

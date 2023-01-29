c50696dcf31239f2d18c88b494d64f27e7d5dea1
Tim <timtran007@gmail.com>
2023-01-29 16:22:54 -0600

    add login form component to page with signup button

--- client/src/Pages/LoginPage.js
+++ client/src/Pages/LoginPage.js
@@ -1,9 +1,22 @@
+//Page that holds the login form
+
+
 import React from "react";
+import LoginForm from "../Components/LoginForm";
+
 
 function LoginPage(){
+
+    function handleButtonClick(){
+
+    }
+
     return(
         <div>
-
+            <LoginForm />
+            <button onClick={handleButtonClick}>
+                Sign Up
+            </button>
         </div>
     )
 }

1535606477bcd477864c4ab0101ca56b39ac5b05
Tim <timtran007@gmail.com>
2023-01-30 09:33:09 -0600

    add ReviewCard component and restaurant and reviews props

--- client/src/Components/RestaurantCard.js
+++ client/src/Components/RestaurantCard.js
@@ -1,9 +1,11 @@
 import React from "react";
+import ReviewCard from "./ReviewCard";
 
-function RestaurantCard(){
+function RestaurantCard({restaurant, reviews}){
+
     return(
         <div>
-
+            <ReviewCard />
         </div>
     )
 }

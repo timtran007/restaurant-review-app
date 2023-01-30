7b967ddf84140d2abc9aadd60c03726861f6e169
Tim <timtran007@gmail.com>
2023-01-30 00:36:56 -0600

    add input fields for sign up form

--- client/src/Components/SignupForm.js
+++ client/src/Components/SignupForm.js
@@ -3,7 +3,49 @@ import React from "react";
 function SignupForm(){
     return(
         <form>
-            
+            <div>
+                <label htmlFor='name'>Name</label>
+                <p> 
+                    <input 
+                        type="text"
+                        name="name"
+                        placeholder="enter name"
+                    />
+                </p>
+            </div>
+            <div>
+                <label htmlFor="username">Username:</label>
+                <p>
+                    <input
+                        type="text"
+                        name="username"
+                        placeholder="enter username"
+                    />
+                </p>
+            </div>
+            <div>
+                <label htmlFor="password">Password:</label>
+                <p>
+                    <input
+                        type="text"
+                        name="password"
+                        placeholder="enter password"
+                    />
+                </p>
+            </div>
+            <div>
+                <label htmlFor="passwordConfirmation">Password Confirmation:</label>
+                <p>
+                    <input
+                        type="text"
+                        name="passwordConfirmation"
+                        placeholder="confirm password"
+                    />
+                </p>
+            </div>
+            <div>
+                <input type="submit" value="Sign up"/>
+            </div>
         </form>
     )
 }

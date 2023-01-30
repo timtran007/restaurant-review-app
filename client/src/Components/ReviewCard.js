0e1002d98f6e2b38f8293a2dfa6273e69e02acf5
Tim <timtran007@gmail.com>
2023-01-30 16:01:59 -0600

    add review information

--- client/src/Components/ReviewCard.js
+++ client/src/Components/ReviewCard.js
@@ -1,9 +1,21 @@
 import React from "react";
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import Image from 'react-bootstrap/Image'
 
-function ReviewCard(){
+function ReviewCard({review}){
+    
     return(
         <div>
-
+            <Card>
+                <Card.Title>{review.headline}</Card.Title>
+                <Card.Text>
+                    ☆{review.rating}
+                    <p>{review.comment}</p>
+                </Card.Text>
+                <Card.Subtitle>- {review.user_name}</Card.Subtitle>
+                
+            </Card>
         </div>
     )
 }

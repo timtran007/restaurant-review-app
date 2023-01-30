95ff24652e44612f04bd2d885917445bc1a9c0f6
Tim <timtran007@gmail.com>
2023-01-30 09:36:03 -0600

    add RestaurantsContainer component

--- client/src/Components/UserContainer.js
+++ client/src/Components/UserContainer.js
@@ -1,9 +1,12 @@
+//house all the restaurants that are associated with the logged in user
+
 import React from "react";
+import RestaurantsContainer from "./RestaurantsContainer";
 
 function UserContainer(){
     return(
         <div>
-
+            <RestaurantsContainer />
         </div>
     )
 }

d3be627637df9d2d75f506e56f04041d257475ad
Tim <timtran007@gmail.com>
2023-01-30 15:54:47 -0600

    add information to restaurant card

--- client/src/Components/RestaurantCard.js
+++ client/src/Components/RestaurantCard.js
@@ -1,11 +1,37 @@
 import React from "react";
 import ReviewCard from "./ReviewCard";
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 function RestaurantCard({restaurant, reviews}){
-
+    
     return(
         <div>
-            <ReviewCard />
+        <Card>
+            <Card.Img variant="top" src={restaurant.image_url} />
+            <Card.Body>
+              <Card.Title>{restaurant.name}</Card.Title>
+              <Card.Text>
+                {restaurant.address}
+                <p>
+                    {restaurant.cuisine} | {restaurant.price} | ☆{restaurant.restaurant_rating} ({restaurant.number_of_reviews} reviews)
+                </p>
+              </Card.Text>
+            </Card.Body>
+            <div>
+                <Button size="sm" variant="primary">Read the Reviews</Button>{' '}
+            </div>
+            {reviews.map(review => {
+                return(
+                    <ReviewCard 
+                        key={review.id}
+                        review={review}
+                    />
+                )
+            })}
+            
+        </Card>
+        
         </div>
     )
 }

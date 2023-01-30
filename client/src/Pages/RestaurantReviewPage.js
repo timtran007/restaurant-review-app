d000fe245cbed2871175cb4c2e170827437cc7f8
Tim <timtran007@gmail.com>
2023-01-29 19:48:28 -0600

    add RestaurantsContainer component to RestaurantReviewPage

--- client/src/Pages/RestaurantReviewPage.js
+++ client/src/Pages/RestaurantReviewPage.js
@@ -1,11 +1,12 @@
 //intention of the page is to show state of the restaurant detail page with all reviews... get request for /restaurants/:id/reviews
 
 import React from "react";
+import RestaurantsContainer from "../Components/RestaurantsContainer";
 
 function RestaurantReviewPage() {
     return(
         <div>
-
+            <RestaurantsContainer />
         </div>
     )
 }

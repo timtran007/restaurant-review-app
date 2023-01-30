a5346351b8847ed4de452abc661d945881385b65
Tim <timtran007@gmail.com>
2023-01-30 01:28:30 -0600

    add useEffect to fetch get request to all restaurants, pass allRestaurants down as a prop

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Route, Switch} from 'react-router-dom';
 import './App.css';
 import LoginPage from './Pages/LoginPage';
@@ -8,20 +8,30 @@ import UserPage from './Pages/UserPage';
 import RestaurantReviewPage from './Pages/RestaurantReviewPage';
 
 //Things to do:
-// create state to house all restaurant information
-// write useEffect to do get fetch requests for all restaurants localhost:3000/restaurants
+
+//localhost:3000/restaurants
 // create state for user log in information to pass to sessions
 // pass props down to respective components
 
 
 function App() {
+  // create state to house all restaurant information
   const [allRestaurants, setAllRestaurants] = useState([])
+
+  //write useEffect to do get fetch requests for all restaurants 
+  useEffect(() =>{
+    fetch('/restaurants')
+    .then(resp => resp.json())
+    .then(data => setAllRestaurants(data))
+  }, [])
+  debugger
+
   return (
     <div className="App">
         <Nav />
         <Switch>
           <Route exact path='/'>
-            <RestaurantReviewPage />
+            <RestaurantReviewPage allRestaurants={allRestaurants}/>
           </Route>
           <Route path='/signup'>
             <SignupPage />

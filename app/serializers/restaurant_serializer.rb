6dcdadb68a60ae0acfb3b32f9cfa2c31b7dc5295
Tim <timtran007@gmail.com>
2023-01-30 15:55:25 -0600

    update to include custom method for get fetch requst

--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,5 +1,23 @@
 class RestaurantSerializer < ActiveModel::Serializer
-  attributes :id, :name, :address, :image_url, :cuisine, :price
+  attributes :id, :name, :address, :image_url, :cuisine, :price, :restaurant_rating, :number_of_reviews
+
+  def price
+    if object.price == 1
+      "$"
+    elsif object.price == 2
+      "$$"
+    elsif object.price == 3
+      "$$$"
+    end
+  end
+
+  def restaurant_rating
+    object.reviews.average(:rating).to_f
+  end
+
+  def number_of_reviews
+    object.reviews.length
+  end
 
   has_many :reviews, serializer: UserReviewSerializer
 end

3b620f808cfc000a26f492060b1d5b60c483f32d
Tim <timtran007@gmail.com>
2023-01-29 19:46:59 -0600

    update name of function in ReviewsContainer and add reviews container in UserReviewsPage

--- client/src/Pages/UserReviewsPage.js
+++ client/src/Pages/UserReviewsPage.js
@@ -1,9 +1,11 @@
+//intention of this page is to fetch get request for user.reviews, so that logged in users can delete or update their reviews
 import React from "react";
+import ReviewsContainer from "../Components/ReviewsContainer";
 
 function UserReviewsPage(){
     return(
         <div>
-
+            <ReviewsContainer />
         </div>
     )
 }

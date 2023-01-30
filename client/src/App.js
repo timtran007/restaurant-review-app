a955b5c398576e4320039963331b4363d0c6ee27
Tim <timtran007@gmail.com>
2023-01-30 01:21:00 -0600

    update RestaurantContainer to RestaurantReviewPage, import useState, create allRestaurants state

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,27 @@
-//will house the get fetch request for all restaurants
-import {Route, Switch} from 'react-router-dom'
+import React, {useState} from 'react';
+import {Route, Switch} from 'react-router-dom';
 import './App.css';
-import RestaurantsContainer from './Components/RestaurantsContainer';
 import LoginPage from './Pages/LoginPage';
 import Nav from './Components/Nav';
-import SignupPage from './Pages/SignupPage'
+import SignupPage from './Pages/SignupPage';
 import UserPage from './Pages/UserPage';
+import RestaurantReviewPage from './Pages/RestaurantReviewPage';
+
+//Things to do:
+// create state to house all restaurant information
+// write useEffect to do get fetch requests for all restaurants localhost:3000/restaurants
+// create state for user log in information to pass to sessions
+// pass props down to respective components
+
 
 function App() {
+  const [allRestaurants, setAllRestaurants] = useState([])
   return (
     <div className="App">
         <Nav />
         <Switch>
           <Route exact path='/'>
-            <RestaurantsContainer />
+            <RestaurantReviewPage />
           </Route>
           <Route path='/signup'>
             <SignupPage />

16954e1075d9c6b6f40326ada7759d2a5771a0dd
Tim <timtran007@gmail.com>
2023-01-29 19:49:02 -0600

    add UserContainer component to UserPage

--- client/src/Pages/UserPage.js
+++ client/src/Pages/UserPage.js
@@ -1,8 +1,11 @@
+//intention of this component is to fetch all restaurants associated with user after signing into the platform
 import React from "react";
+import UserContainer from "../Components/UserContainer";
 
 function UserPage(){
     return(
         <div>
+            <UserContainer />
 
         </div>
     )

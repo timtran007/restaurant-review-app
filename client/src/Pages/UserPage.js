7680bd751690db0b615af887e3232f2005def8fa
Tim <timtran007@gmail.com>
2023-01-31 15:43:41 -0600

    map user restaurants to pass down as prop to UserRestaurantCard, add ternary expression if user is not loaded yet

--- client/src/Pages/UserPage.js
+++ client/src/Pages/UserPage.js
@@ -1,12 +1,26 @@
-//intention of this component is to fetch all restaurants associated with user after signing into the platform
-import React from "react";
-import UserContainer from "../Components/UserContainer";
+import React, {useEffect, useState} from "react";
+import UserRestaurantsCard from "../Components/UserRestaurantsCard";
 
-function UserPage(){
+//intention of this component is to fetch all restaurants associated with user after signing into the platform
+//create useEffect fetch for user.restaurants or /me
+
+//add a nested route for reviews
+//https://learning.flatironschool.com/courses/5650/assignments/207891?module_item_id=479514
+
+function UserPage({user}){
     return(
         <div>
-            <UserContainer />
-
+            <h2>My Restaurants</h2>
+            {!user ? "Loading" : user.restaurants.map( restaurant => {
+                return(
+                    <UserRestaurantsCard 
+                        key={restaurant.id}
+                        user={user}
+                        restaurant={restaurant}
+                    />
+                )
+            })}
+            
         </div>
     )
 }

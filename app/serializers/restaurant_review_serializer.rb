4a5374be0f484300d511591ba8fb1c15fbd0ee29
Tim <timtran007@gmail.com>
2023-01-30 22:04:47 -0600

    update serializer to include custom methods

--- app/serializers/restaurant_review_serializer.rb
+++ app/serializers/restaurant_review_serializer.rb
@@ -1,6 +1,24 @@
 class RestaurantReviewSerializer < ActiveModel::Serializer
   #what we want to see from the restaurants
-  attributes :id, :name, :address, :image_url, :cuisine, :price
+  attributes :id, :name, :address, :image_url, :cuisine, :price, :restaurant_rating, :number_of_reviews
+
+  def price
+    if object.price == 1
+      "$"
+    elsif object.price == 2
+      "$$"
+    elsif object.price == 3
+      "$$$"
+    end
+  end
+
+  def restaurant_rating
+    object.reviews.average(:rating).to_f
+  end
+
+  def number_of_reviews
+    object.reviews.length
+  end
 
   has_many :reviews
 

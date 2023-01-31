160e2a66052a3c135c942ab4731ee20b12b25a3a
Tim <timtran007@gmail.com>
2023-01-31 15:40:17 -0600

    add user prop to LoginForm

--- client/src/Pages/LoginPage.js
+++ client/src/Pages/LoginPage.js
@@ -6,12 +6,12 @@ import LoginForm from "../Components/LoginForm";
 import { Link } from "react-router-dom";
 
 
-function LoginPage({onLogin}){
+function LoginPage({onLogin, user}){
 
     return(
         <div>
             <h2>Log Into Your Account</h2>
-            <LoginForm onLogin={onLogin}/>
+            <LoginForm onLogin={onLogin} user={user}/>
             <p>
                 Don't have an account yet? <Link to='/signup'>Sign up here</Link>!
             </p>

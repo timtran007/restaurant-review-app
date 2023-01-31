4bd300b414841c440cf704345a558d11b8e60215
Tim <timtran007@gmail.com>
2023-01-31 15:37:03 -0600

    add JSX to render data

--- client/src/Components/ReviewsContainer.js
+++ client/src/Components/ReviewsContainer.js
@@ -1,11 +1,31 @@
+//hosts all the reviews associated with logged in user...
 import React from "react";
+import Card from 'react-bootstrap/Card'
+import EditReviewForm from "./EditReviewForm";
+import DeleteReview from "./DeleteReview";
 
-function ReviewsContainer(){
-    return(
-        <div>
-            
-        </div>
-    )
+function ReviewsContainer({review, user, onEditReview, onDeleteReview}){
+    console.log("reviewscontainer", review)
+            return(
+                <div>
+                    <Card>
+                        <Card.Title>{review.headline}</Card.Title>
+                        <Card.Text>
+                            {review.restaurant.name}
+                        </Card.Text>
+                        <Card.Text>
+                            ☆{review.rating}
+                        </Card.Text>
+                        <Card.Text>
+                            {review.comment}
+                        </Card.Text>
+                        <EditReviewForm onEditReview={onEditReview}/>
+                        <div>
+                        <DeleteReview onDelete={onDeleteReview}/>
+                        </div>
+                    </Card>
+                </div>
+            )
 }
 
 export default ReviewsContainer

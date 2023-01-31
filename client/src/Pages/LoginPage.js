af650b07884d0029f87ba53b2d293c7bae2b8632
Tim <timtran007@gmail.com>
2023-01-31 00:42:55 -0600

    add prop of onLogin

--- client/src/Pages/LoginPage.js
+++ client/src/Pages/LoginPage.js
@@ -6,12 +6,12 @@ import LoginForm from "../Components/LoginForm";
 import { Link } from "react-router-dom";
 
 
-function LoginPage(){
+function LoginPage({onLogin}){
 
     return(
         <div>
             <h2>Log Into Your Account</h2>
-            <LoginForm />
+            <LoginForm onLogin={onLogin}/>
             <p>
                 Don't have an account yet? <Link to='/signup'>Sign up here</Link>!
             </p>

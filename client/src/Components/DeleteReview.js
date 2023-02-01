4da1c7dbae52062fe7264bbb9cc9bc9f541a3cb7
Tim <timtran007@gmail.com>
2023-01-31 19:45:29 -0600

    add function handleDelete and setting state through callback

--- client/src/Components/DeleteReview.js
+++ client/src/Components/DeleteReview.js
@@ -3,7 +3,16 @@ import Button from 'react-bootstrap/Button'
 
 function DeleteReview({onDeleteReview, review}){
 
-    
+    function handleDelete(){
+        fetch(`/reviews/${review.id}`, {
+            method: "DELETE"
+        })
+        .then(resp => {
+            if (resp.ok){
+                resp.json().then( deleted => onDeleteReview(deleted))
+            }
+        })
+    }
 
     return(
         <Button onClick={handleDelete} variant='secondary' size='sm'>

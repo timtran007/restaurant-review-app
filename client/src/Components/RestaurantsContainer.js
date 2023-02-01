500e0b60d8820296718dcf0533869f5e7cd01e79
Tim <timtran007@gmail.com>
2023-01-31 22:07:17 -0600

    pass props down

--- client/src/Components/RestaurantsContainer.js
+++ client/src/Components/RestaurantsContainer.js
@@ -1,7 +1,9 @@
 import React from "react";
 import RestaurantCard from '../Components/RestaurantCard';
 
-function RestaurantsContainer({allRestaurants}) {
+//pass user props down to Restaurant Card
+
+function RestaurantsContainer({allRestaurants, user, onCreateReview}) {
     return(
         <div>
             {allRestaurants.map(restaurant => {
@@ -10,6 +12,8 @@ function RestaurantsContainer({allRestaurants}) {
                     key={restaurant.id}
                     restaurant={restaurant}
                     reviews={restaurant.reviews}
+                    user={user}
+                    onCreateReview={onCreateReview}
                 />)
             }) 
             }

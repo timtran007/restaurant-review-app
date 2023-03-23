0903759747a468be1a1aa40dcdce7260eab93c33
Tim <timtran007@gmail.com>
2023-03-23 14:51:51 -0500

    updates routes and fallback controller

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
